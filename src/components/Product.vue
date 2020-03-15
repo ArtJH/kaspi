791e6f191f35fa8c67057809a907878c2ac0b5f7
Tsoy Arthur <artur23111997@gmail.com>
2020-03-16 01:15:42 +0600

    page cart finish

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,5 +1,25 @@
 <template>
-    $END$
+    <div class="product-wrapper">
+       <div class="productLeft">
+           <img class="productLeft-img" src="../assets/img/product.png" alt="">
+       </div>
+        <div class="productRight">
+            <p class="productRight-title">Название продукта</p>
+            <p class="productRight-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+            <div class="productFooter">
+                <div class="productFooter-counter">
+                    <i class="fas btn-counter fa-minus"></i>
+<!--                    <button class="btn-counter">-</button>-->
+                    <p class="productFooter-number">1</p>
+<!--                    <button class="btn-counter">+</button>-->
+                    <i class="fas btn-counter fa-plus"></i>
+                </div>
+                <div class="productFooter-price">
+                    <p class="productFooter-price__p">$390.50</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
---
+++ src/views/Cart.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="cart-wrapper">
+        <div class="cart-header container">
+            <i class="fas exist-ic fa-times"></i>
+            <div class="cart-header-title">
+                <h2 class="cart-header-title__h2">Your Cart</h2>
+            </div>
+            <button class="cart-header-btn">
+                Clear All
+            </button>
+        </div>
+        <div class="content container">
+            <Product/>
+            <Product/>
+            <div class="miniDesc">
+                <p class="miniDesc-p">Lorem Ipsum is simply dummy text of the printing and typesetting indu</p>
+                <p class="miniDesc-p">Lorem Ipsum is simply dummy text of the printing and typesetting indu 1-4 days</p>
+            </div>
+        </div>
+        <div class="cart-footer">
+            <div class="cart-footer-wrapper container">
+                <div class="row-desc">
+                    <p class="row-desc__p">Subtotal</p>
+                    <p class="row-desc__p">$495.49</p>
+                </div>
+                <div class="row-desc">
+                    <p class="row-desc__p">Estimates Tax</p>
+                    <p class="row-desc__p">$10.87</p>
+                </div>
+                <hr class="line-hr">
+                <div class="row-desc-total">
+                    <p class="row-desc-total__p">Total</p>
+                    <p class="row-desc-total__p">$506.36</p>
+                </div>
+                <div class="footer-bootom">
+                    <div class="footer-ask">
+                        <p class="footer-ask__p">
+                            Questions? <a href="tel:(771)8431730" class="blue">(771)843-1730</a>
+                        </p>
+                    </div>
+                    <div class="footer-btn">
+                        <button class="footer-btn__btn">Checkout</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+    import Product from "../components/Product";
+    export default {
+        name: "cart",
+        components: {
+            Product
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
